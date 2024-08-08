c3fc1232bb2324fecc7436bafbe00b4b093f578f
= <=>
2024-08-09 03:55:09 +0530

    update1

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-from dotenv import load_dotenv
+import config
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+import routes
+
+import models
 
 if __name__ == '__main__':
     app.run(debug=True)
